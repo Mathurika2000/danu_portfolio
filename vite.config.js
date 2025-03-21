d6311482adc996b69c062f36ddf7620a0397f1fc
unknown <jesumathu11@gmail.com>
2025-03-21 10:51:26 +0530

    danu6

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-site/',  // Corrects asset paths for GitHub Pages
+  base: '/danu_portfolio/',  // Corrects asset paths for GitHub Pages
   build: {
     outDir: 'build',  // Specifies the output directory for the build
   },
